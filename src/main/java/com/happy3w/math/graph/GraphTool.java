fbfcf86caa1e581c4a5995d5d6bf77d6b37103ec
ysgao <ysgao@thoughtworks.com>
2022-12-03 17:30:20 +0800

    chore: support load nodes with layer limit

--- src/main/java/com/happy3w/math/graph/GraphTool.java
+++ src/main/java/com/happy3w/math/graph/GraphTool.java
@@ -7,8 +7,10 @@ import lombok.Getter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Queue;
 import java.util.Set;
@@ -19,13 +21,23 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class GraphTool {
-    public static <NK, NV, EK, EV> DirectGraph<NK, NV, EK, EV> buildGraph(List<NK> startNodeIds,
-                                                                   Function<List<NK>, List<GraphNode<NK, NV, EK, EV>>> loader) {
+    public static <NK, NV, EK, EV> DirectGraph<NK, NV, EK, EV> buildGraph(
+            List<NK> startNodeIds,
+            Function<List<NK>, List<GraphNode<NK, NV, EK, EV>>> loader) {
+        return buildLimitGraph(startNodeIds, loader, -1);
+    }
+
+    public static <NK, NV, EK, EV> DirectGraph<NK, NV, EK, EV> buildLimitGraph(
+            List<NK> startNodeIds,
+            Function<List<NK>, List<GraphNode<NK, NV, EK, EV>>> loader,
+            int layLimit) {
         DirectGraph<NK, NV, EK, EV> directGraph = new DirectGraph<>();
 
         Set<NK> existIds = new HashSet<>();
-        for (List<NK> nodesToLoad = new ArrayList<>(startNodeIds);
-             !nodesToLoad.isEmpty();) {
+        List<NK> nodesToLoad = new ArrayList<>(startNodeIds);
+        for (int times = 0;
+             !nodesToLoad.isEmpty() && (layLimit <= 0 || times <= layLimit);
+             times++) {
             List<GraphNode<NK, NV, EK, EV>> newNodes = loader.apply(nodesToLoad);
             directGraph.acceptNodes(newNodes);
 
@@ -37,6 +49,20 @@ public class GraphTool {
                     .collect(Collectors.toList());
         }
 
+        if (!nodesToLoad.isEmpty()) {
+            directGraph.nodeStream().forEach(node -> {
+                Map<EK, GraphEdge<EK, EV, NK>> outcomes = node.getOutcomes();
+                if (outcomes != null) {
+                    Iterator<GraphEdge<EK, EV, NK>> edgeIt = outcomes.values().iterator();
+                    while (edgeIt.hasNext()) {
+                        GraphEdge<EK, EV, NK> edge = edgeIt.next();
+                        if (directGraph.node(edge.getTo()) == null) {
+                            edgeIt.remove();
+                        }
+                    }
+                }
+            });
+        }
         return directGraph;
     }
 
